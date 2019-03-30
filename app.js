0b581d14eb62f117a39ba31843ce7affc81e3b82
Frank Moreno <Frank.Moreno95@gmail.com>
2019-03-30 17:35:11 -0500

    Add Procfile for heroku

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 const data = require('./data.json');
 
+app.get('/', (req, res) => {
+    res.send('We\'re up!');
+})
 app.get('/cards/:cardName', (req, res) => {
     let cardName = req.params.cardName;
     res.json(data[cardName]);
+});
+
+app.get('/cards/', (req, res) => {
+    res.json(data);
 });
 
 app.listen(port, ()=> {
