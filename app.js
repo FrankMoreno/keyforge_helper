8d8fbf6b07902cd432841ed74cf1beb98db665e6
Frank Moreno <Frank.Moreno95@gmail.com>
2019-03-31 18:11:50 -0500

    Add fullfillment logic to app

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const expressApp = express().use(bodyParser.json());
+
+const app = dialogflow({debug: true});
+const {dialogflow} = require('actions-on-google');
+
 const port = process.env.PORT || 9000;
 const data = require('./data.json');
 
-app.get('/', (req, res) => {
+app.intent('Keyforge Card', (conv, {Card}) => {
+    conv.close(Card);
+});
+
+expressApp.get('/', (req, res) => {
     res.send('We\'re up!');
 })
-app.get('/cards/:cardName', (req, res) => {
+expressApp.get('/cards/:cardName', (req, res) => {
     let cardName = req.params.cardName;
     res.json(data[cardName]);
 });
 
-app.get('/cards/', (req, res) => {
+expressApp.get('/cards/', (req, res) => {
     res.json(data);
 });
 
-app.listen(port, ()=> {
+expressApp.post('/fulfillment', app);
+
+expressApp.listen(port, ()=> {
     console.log(`Example app listening on port ${port}!`);
 });
