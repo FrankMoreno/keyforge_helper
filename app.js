3a8dba215e6111a4e66921c28a691807ca8527fa
Frank Moreno <Frank.Moreno95@gmail.com>
2019-03-31 19:12:44 -0500

    Is this working?

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const data = require('./data.json');
 app.intent('Keyforge Card', (conv, {Card}) => {
     let cardName = Card.toLowerCase();
     let cardInfo = data[cardName];
-    // let responseString = `Type: ${cardinfo['Type']}.
-    // House: ${cardinfo['House']}.
-    // Card Description: ${cardInfo['Card Text']}.
-    // Aember: ${cardInfo['\u00c6mber']}.`;
-
-    conv.close('Type: ' + cardinfo['Type'] + '. House: ' + 
-    cardinfo['House'] + '. Card Description: ' + 
-    cardInfo['Card Text'] + '. Aember: ' + 
-    cardInfo['\u00c6mber'] + '.');
+    let responseString = `Type: ${cardinfo['Type']}.
+    House: ${cardinfo['House']}.
+    Card Description: ${cardInfo['Card Text']}.
+    Aember: ${cardInfo['\u00c6mber']}.`;
+    conv.close('Hello, world!');
+    // conv.close('Type: ' + cardinfo['Type'] + '. House: ' + 
+    // cardinfo['House'] + '. Card Description: ' + 
+    // cardInfo['Card Text'] + '. Aember: ' + 
+    // cardInfo['\u00c6mber'] + '.');
 });
 
 expressApp.get('/', (req, res) => {
